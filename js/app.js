e64c297a342c1053677974526aeda9dbeb19dcd1
root <eli.dungan@gmail.com>
2019-05-16 16:34:04 -0700

    added more comments to explain js code

--- js/app.js
+++ js/app.js
@@ -5,31 +5,30 @@ window.onscroll = function() {makeSticky()}; // when the user scrolls the page, 
 /* ---------------------------------------------------Pre-load--------------------------------------------------- */
 /* ----------------------------------------------Global Declarations--------------------------------------------- */
 var grabElement = document.getElementById('stickynav'); // gets the navbar element.
-var sticky = grabElement.offsetTop; // gets the offset position of the navbar.
-var grabList = document.getElementsByClassName('navListItem'); // gets the links in the navbar.
-var transitionTime = '0.6s'; // sets transition time for CSS fade-in fade out animation
+var sticky = grabElement.offsetTop; // gets the offset position of the browser scrollbar.
+var grabList = document.getElementsByClassName('navListItem'); // gets the links from the navbar.
+var transitionTime = '0.6s'; // sets transition time for CSS fade-in fade out hover animation.
 /* ----------------------------------------------Global Declarations--------------------------------------------- */
 /* ----------------------------------------------Nav Bar and Header---------------------------------------------- */
 function makeSticky() { // makes navbar sticky upon scroll
-  var grabElement = document.getElementById('stickynav');
-  var grabList = document.getElementsByClassName('navListItem');
-  var grabHeader = document.getElementById('magic_header')
+  var grabElement = document.getElementById('stickynav'); // gets the navbar element.
+  var grabHeader = document.getElementById('magic_header') // gets the header element.
   if(window.pageYOffset > sticky) { // when scrolling down, makes navbar sticky and transparent while removing the header.
-    grabElement.style.position = 'fixed';
-    grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0.9)';
-    grabElement.style.transition = transitionTime;
-    grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 0)';
-    grabHeader.style.transition = transitionTime;
+    grabElement.style.position = 'fixed'; // changes position of navbar to fixed.
+    grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0.9)'; // makes navbar transparent.
+    grabElement.style.transition = transitionTime; // gets transition time. 
+    grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 0)'; // removes header. 
+    grabHeader.style.transition = transitionTime; // gets transition time. 
   } else if(window.pageXOffset > sticky) { // when scrolling horizontally, maintains the style of the navbar and header.
-      grabElement.style.position = 'fixed';
-      grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0.9)';
-      grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 1)';
+      grabElement.style.position = 'fixed'; // changes position of navbar to fixed. 
+      grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0.9)'; // makes navbar transparent.
+      grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 1)'; // makes header opaque.
   } else { // when scrolled to top position, resets styles of the navbar and header.
-      grabElement.style.position = 'fixed';
-      grabElement.style.backgroundColor = 'rgb(255, 255, 255, 1)';
-      grabElement.style.transition = transitionTime;
-      grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 1)';
-      grabHeader.style.transition = transitionTime;
+      grabElement.style.position = 'absolite'; // changes position of navbar to absolute.
+      grabElement.style.backgroundColor = 'rgb(255, 255, 255, 1)'; // makes navbar opaque. 
+      grabElement.style.transition = transitionTime; // gets transition time.
+      grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 1)'; // makes header opaque.
+      grabHeader.style.transition = transitionTime; // gets transition time. 
   }
 }
 /* ----------------------------------------------Nav Bar and Header---------------------------------------------- */
