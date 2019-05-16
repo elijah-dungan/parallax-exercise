e5e8d29c5dd5b499d3daa54f669093cccd379f53
root <eli.dungan@gmail.com>
2019-05-16 01:05:05 -0700

    used different animation techniques on header

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-/* -----------------------------------------------Nav Bar + Header----------------------------------------------- */
-
+/* ---------------------------------------------------Pre-load--------------------------------------------------- */
 makeSticky(); // preloads function to prevent offset of Nav Bar.
+animate(); // preloads function to prevent misfire of animation dependent on page4Offset.
 window.onscroll = function() {makeSticky()}; // when the user scrolls the page, executes makeSticky. 
-
-var nav = document.getElementById('stickynav'); // Get the nav element, id="stickynav".
-
-var sticky = nav.offsetTop; // Get the offset position of the navbar.
-
+/* ---------------------------------------------------Pre-load--------------------------------------------------- */
+/* -------------------------------------------------Declarations------------------------------------------------- */
+var grabElement = document.getElementById('stickynav');
+var sticky = grabElement.offsetTop; // Get the offset position of the navbar.
+var grabList = document.getElementsByClassName('navListItem');
+var transitionTime = '0.6s'; // sets transition time for CSS fade-in fade out animation
+/* -------------------------------------------------Declarations------------------------------------------------- */
+/* ----------------------------------------------------Nav Bar--------------------------------------------------- */
 function makeSticky() { // makes navbar sticky upon scroll. Removes sticky when scroll is returned to beginning y position.
   var grabElement = document.getElementById('stickynav');
   var grabList = document.getElementsByClassName('navListItem');
@@ -16,6 +19,7 @@ function makeSticky() { // makes navbar sticky upon scroll. Removes sticky when 
     grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0.9)';
     grabElement.style.transition = transitionTime;
     grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 0)';
+    grabHeader.style.transition = transitionTime;
     for(var i = 0; i < grabList.length; i++) {
       grabList[i].style.color = 'black';
     } 
@@ -25,25 +29,21 @@ function makeSticky() { // makes navbar sticky upon scroll. Removes sticky when 
       grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 1)';
       for(var i = 0; i < grabList.length; i++) {
         grabList[i].style.color = 'white';
-      }
-  } else {
-    grabElement.style.position = 'fixed';
-    grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0)';
-    grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 1)';
-    grabElement.style.transition = transitionTime;
-    for(var i = 0; i < grabList.length; i++) {
-      grabList[i].style.color = 'white';
     }
-  }
-}
-
-var transitionTime = '0.6s'; // sets transition time for CSS fade-in fade out animation
-
-function makeOpaque() { // makes navbar opaque, used with onmouseover.
-  if (window.pageYOffset < nav.offsetTop) {
   } else {
-    var grabElement = document.getElementById('stickynav');
-    var grabList = document.getElementsByClassName('navListItem');
+      grabElement.style.position = 'fixed';
+      grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0)';
+      grabElement.style.transition = transitionTime;
+      grabHeader.style.backgroundColor = 'rgb(151, 61, 96, 1)';
+      grabHeader.style.transition = transitionTime;
+      for(var i = 0; i < grabList.length; i++) {
+        grabList[i].style.color = 'white';
+      }
+    }
+}
+function makeOpaque() { // makes navbar opaque, used with onmouseover.
+  if (window.pageYOffset < sticky) {
+  } else {
     grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0.9)';
     grabElement.style.transition = transitionTime;
     for(var i = 0; i < grabList.length; i++) {
@@ -51,12 +51,9 @@ function makeOpaque() { // makes navbar opaque, used with onmouseover.
     }
   }
 }
-
 function makeTransparent() { // makes navbar transparent, used with onmouseout.
-  if (window.pageYOffset > nav.offsetTop) {
+  if (window.pageYOffset > sticky) {
   } else {
-    var grabElement = document.getElementById('stickynav');
-    var grabList = document.getElementsByClassName('navListItem');
     grabElement.style.backgroundColor = 'rgb(255, 255, 255, 0)';
     grabElement.style.transition = transitionTime;
     for(var i = 0; i < grabList.length; i++) {
@@ -64,28 +61,32 @@ function makeTransparent() { // makes navbar transparent, used with onmouseout.
     }
   }
 }
+/* ----------------------------------------------------Nav Bar--------------------------------------------------- */
 
-/* -----------------------------------------------Nav Bar + Header----------------------------------------------- */
-
+function animate() {
 /* --------------------------------------------------Sub Header-------------------------------------------------- */
-
-$(window).scroll(function() { // gets scroll position and multiplies by number to increase/decrease speed, adds this as px to CSS property.
-  var scroll_position = $(window).scrollTop() * 0.50; // change rate of speed here.
-  $('body').css({
-    'background-position-y' : - scroll_position + 'px',
+  $(window).scroll(function() { // gets scroll position and multiplies by number to increase/decrease speed, adds this as px to CSS property.
+    var scroll_position = $(window).scrollTop() * 0.50; // change rate of speed here.
+    $('body').css({
+      'background-position-y' : - scroll_position + 'px',
+    })
   })
-})
-
 /* --------------------------------------------------Sub Header-------------------------------------------------- */
-
 /* -----------------------------------------------------Logo----------------------------------------------------- */
-
-$(window).scroll(function() { // gets scroll position and multiplies by number to fade, changes alpha to CSS property.
-  var scroll_position = $(window).scrollTop() / -550; // change rate of fade here.
-  $('#logo').css({
-    'opacity' : ++ scroll_position, 
-    //'filter' : ' alpha(opacity=' + ++ scroll_position * 100 + ');'
+  $(window).scroll(function() { // gets scroll position and multiplies by number to fade, changes alpha to CSS property.
+    var scroll_position = $(window).scrollTop() / -450; // change rate of fade here.
+    $('#logo').css({
+      'opacity' : ++ scroll_position, 
+      'filter' : ' alpha(opacity=' + ++ scroll_position * 100 + ')'
+    })
   })
-})
-
 /* -----------------------------------------------------Logo----------------------------------------------------- */
+/* ----------------------------------------------------header---------------------------------------------------- 
+  $(window).scroll(function() { // gets scroll position and multiplies by number to fade, changes alpha to CSS property.
+    var scroll_position = $(window).scrollTop() / -75; // change rate of fade here.
+    $('#magic_header').css({
+      'background-color' : 'rgba(151,61,96,' + ++ scroll_position + ')'
+    })
+  })
+ ----------------------------------------------------header---------------------------------------------------- */
+}
